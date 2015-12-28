7b29d59f88ecd1d19685adbd9b31fb945c3724ad
Hagen Salewski <salewski@wiwi.uni-kl.de>
2015-12-28 11:37:10 +0100

    Added A Control Program for TimeToTarget Search. This is going to be the final application

---
+++ algorithm/application/GQAP_GRASP_Solver_TimeToTarget.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <eo>
+#include "GQAP_GRASP_Algo.h"
+
+int main(int argc, char* argv[]) {
+	try {
+		std::cout << "GRASP Solver for the GQAP" << std::endl;
+		std::cout << "compiled at " << __DATE__ << " " << __TIME__ << std::endl;
+		std::cout << "Programming by Hagen Salewski (salewski@wiwi.uni-kl.de), University of Kaiserslautern" << std::endl << std::endl;
+	
+		// Initialize Parser, load and parse parameters from the CLI
+		eoParser parser(argc, argv);
+		parameters param;
+		parseFile(parser, param);
+		
+		// Initialize the RNG with the provided seed
+		rng.reseed(param.seed);
+		
+		// check if parameters are set to target value or if one should be found first
+		switch(param.stoppingCriterion) {
+			case StoppingCriterionValue:
+			case StoppingCriterionValueIterations:
+			case StoppingCriterionValueTime:
+				break;
+			default:
+				throw std::runtime_error("ERROR. Time2Target-Algorithm: No Target Value or no Valid Combination of Stopping Criterions provided");
+		}
+		
+		// Initialize the Algorithm
+		GQAP_GRASP_Algo grasp(param);
+		
+		grasp.RunTimeToTarget();
+	} 
+	
+	catch(std::exception& e) {
+		std::cout << "Expectation cought: ";
+		std::cout << e.what() << std::endl;
+	}
+	
+	return EXIT_SUCCESS;
+}
