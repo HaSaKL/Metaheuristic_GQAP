402979de57bc642a20649ac4a71bb639fa5698a5
Hagen Salewski <salewski@wiwi.uni-kl.de>
2015-12-28 10:47:07 +0100

    Addded a class for the GRASP-Algorithm. At this point without Path-Relinking, which will be added in future commits

---
+++ algorithm/src/GQAP_GRASP_Algo.cpp
@@ -0,0 +1,242 @@
+#include "GQAP_GRASP_Algo.h"
+
+// C O N S T R U C T O R 
+
+GQAP_GRASP_Algo::GQAP_GRASP_Algo(parameters _param) {
+	
+	// copy the parameters
+	param = _param;
+	
+	// read the problem data and save an arbitrary solution as best
+	p = new GQAP(param.problemFile);
+	
+	// initialize the best Solution to a random solution
+	p->RandomInit();
+	fullEval(*p);
+	bestSol = *p;
+	
+	// Initialize the Incremental Evaluation Function
+	incrEval = new GQAP_ElementFlip_IncrEval(*p);
+	
+	// Calculate Neighborhood Size
+	NeighborhoodSize = p->GetNumEquip() * (p->GetNumLocation() - 1);
+	
+	// setup local search algorithm
+	switch(param.LSStrategy) {
+		case LSStrategyBestImprov:
+			nh = new moOrderNeighborhood<GQAP_ElementFlipIndex_Neighbor>(NeighborhoodSize);
+			ls = new moSimpleHC<GQAP_ElementFlipIndex_Neighbor>(*nh, fullEval, *incrEval);
+			break;
+			
+		case LSStrategyRandomImprov:
+			nh = new moRndWithoutReplNeighborhood<GQAP_ElementFlipIndex_Neighbor>(NeighborhoodSize);
+			ls = new moFirstImprHC<GQAP_ElementFlipIndex_Neighbor>(*nh, fullEval, *incrEval);
+			break;
+			
+		case LSStrategyNone:
+			ls = new moDummyLS<GQAP_ElementFlipIndex_Neighbor>(fullEval);
+			break;
+	}
+	
+	// setup the continuator
+	switch(param.stoppingCriterion) {
+		case StoppingCriterionIterations:
+			cont = new moIterContinuator<GQAP_ElementFlipIndex_Neighbor>(param.maxIterations);
+			break;
+		
+		case StoppingCriterionRuntime:
+			cont = new moTimeContinuator<GQAP_ElementFlipIndex_Neighbor>(param.maxRuntime);
+			break;
+			
+		case StoppingCriterionValue:
+			cont = new moFitContinuator<GQAP_ElementFlipIndex_Neighbor>(param.targetValue + pow(10,-6));
+			// Note: Add a Small Epsilonn to compensate float-round-offs during incrEvals
+			break;	
+			
+		case StoppingCriterionValueIterations:
+			// Set Continuator to Iterations First and to Value after it has been found in the Time2Targer-Function
+			cont = new moIterContinuator<GQAP_ElementFlipIndex_Neighbor>(param.maxIterations);
+			break;
+			
+		case StoppingCriterionValueTime:
+			// Set Continuator to Runtime First and to Value after it has been found in the Time2Targer-Function
+			cont = new moTimeContinuator<GQAP_ElementFlipIndex_Neighbor>(param.maxRuntime);
+			break;
+	}
+	
+	// setup the alpha-generator
+	switch(param.StartSol) {
+		case StartSolGRASPFixed:
+			alpha = new GRASP_FixedAlpha(param.fixedAlpha);
+			break;
+			
+		case StartSolGRASPReactive:
+			alpha = new GRASP_ReactiveAlpha(*p, param.maxAlpha, param.minAlpha, param.numAlpha, param.itsReCalcAlpha);
+			break;
+		
+		case StartSolGRASPUniform:
+			alpha = new GRASP_UniformRandomAlpha(param.maxAlpha, param.minAlpha, param.numAlpha);
+			break;
+		
+		case StartSolGreedy:
+			alpha = new GRASP_FixedAlpha(0);
+			break;
+		
+		case StartSolRandom:
+			alpha = new GRASP_FixedAlpha(1);
+			break;
+	}
+	
+	// FIXME: setup the path-relinking, don't forget the solution pool
+}
+
+GQAP_GRASP_Algo::~GQAP_GRASP_Algo() {
+	delete p;
+	delete incrEval;
+	delete cont;
+	delete nh;
+	delete ls;
+	delete alpha;
+	
+	//FIXME: delete the path relinking
+}
+
+
+// R U N N E R S
+
+// Definition of a single Iteration of the GRASP-Algorithm
+void GQAP_GRASP_Algo::GRASPIteration() {
+	
+	// Initialize the a Soluion
+	p->GRASPInit(alpha->operator ()(*p));
+	fullEval(*p);
+	
+	// Do the Local Search
+	ls->operator ()(*p);
+	
+	// FIXME: Add the Path-Relinking!
+}
+
+// Silent run of the algorithm as defined in the parameters. Solution Values and Runtime are saved in the call parameters
+void GQAP_GRASP_Algo::Run(GQAP_Solution & _Sol, double & _Runtime) {
+	
+	// save a random solution as the best solution
+	GQAP_Solution tmpSol;
+	
+	// setup timers
+	clock_t t;
+	double elapsedTime;
+	
+	// init stopping criterion
+	cont->init(*p);
+	
+	// init alpha generator
+	alpha->init(*p);
+	
+	//start timer
+	t = clock();
+	
+	// start the algorithm
+	do {
+		
+		GRASPIteration();
+		tmpSol = *p;
+		
+		if(tmpSol > bestSol) {
+			bestSol = tmpSol;
+		}
+		
+	} while(cont->operator ()(*p));
+	
+	// calculate running time in ms
+	t = clock_t() - t;
+	elapsedTime = double(t) *1000 / (CLOCKS_PER_SEC);
+	
+	// save results in function paramerters
+	_Runtime = elapsedTime;
+	_Sol = bestSol;
+}
+	
+// noisy run of the algorithm (with some output)	
+void GQAP_GRASP_Algo::Run() {
+	double Runtime;
+	GQAP_Solution Sol;
+	
+	Run(Sol, Runtime);
+	
+	// print result
+	std::cout << "Solution Value: " << Sol.fitness() << ", Time: " << Runtime << std::endl;
+}
+	
+// Implementation of time-to-target runs including the output format
+void GQAP_GRASP_Algo::RunTimeToTarget() {
+	
+	// Initialize variables
+	unsigned int itCounter = 0;		// counts iterations until target value has been found
+	clock_t t;						// clock ticker
+	double timeToTarget;			// holds runtime of singel t2t-runs
+	
+	// Open Output File and write header of results table if file is empty
+	std::ofstream resultsFile;
+	resultsFile.open(param.resultsFile.c_str(), std::ios::app);
+	if (resultsFile.tellp() == 0) {	// if file is empty
+		resultsFile << "Problem; StartSol; LSStrategy; PRMeth; PRDir; PRPoolSelect; Time; Iterations" << std::endl;
+	}
+	
+	// Print header for console output
+	std::cout << "Time; Iterations; Repetition" << std::endl;
+	
+	// See if a target has to been found first
+	// in this case the constructor did already set the valid 
+	// stopping criterion. The value needs to be calculated and set as
+	// new stopping criterion
+	if ( (param.stoppingCriterion == StoppingCriterionValueIterations) || (param.stoppingCriterion == StoppingCriterionValueTime)) {
+		
+		// Run Algorithm Once to find a Solution
+		GQAP_Solution tmpSol;
+		double tmpTime;
+		Run(tmpSol, tmpTime);
+		
+		// delete the old continuator and set a new value continuator for time-to-target
+		delete cont;
+		cont = new moFitContinuator<GQAP_ElementFlipIndex_Neighbor>(tmpSol.fitness() + pow(10,-6));
+	}
+	
+	// do time-to-target runs for defined number of repetitions
+	for (int i = 0; i < param.numRepetition; i++) {
+		
+		// (re-)initialize continuator and alpha-generator for next time-to-target run
+		cont->init(*p);
+		alpha->init(*p);
+		
+		// reset counter and start clock
+		itCounter = 0;
+		t = clock();
+		
+		// run time to target until target value has been found. No solution comparision needed
+		// FIXME: If a better solution than target has been found: It should be saved!
+		do {
+			GRASPIteration();
+			itCounter++;
+		} while(cont->operator ()(*p));
+		
+		// calculate time-to-target in ms
+		t = clock() - t;
+		timeToTarget = double(t) * 1000 / (CLOCKS_PER_SEC);
+		
+		// print result
+		std::cout << timeToTarget << " ms; \t " << itCounter << " iterations; \t repetition " << i << std::endl;
+		
+		// write results to file
+		resultsFile << param.problemFile << "; " << param.StartSol << "; " << param.LSStrategy << "; ";
+		resultsFile << param.PRMeth << "; " << param.PRDir << "; " << param.PRPoolSelect << "; ";
+		resultsFile << timeToTarget << "; " << itCounter << std::endl;
+	}
+	
+	// close results file
+	resultsFile.close();
+}
+
+
+// S W I T C H E S
+
---
+++ algorithm/src/GQAP_GRASP_Algo.h
@@ -0,0 +1,87 @@
+#ifndef GQAP_GRASP_Algo_H
+#define GQAP_GRASP_Algo_H
+
+// solution and problem representation and evaluation functions
+#include "GQAP_Solution.h"
+#include "GQAP.h"
+#include "GQAP_Eval.h"
+#include "GQAP_ElementFlip_IncrEval.h"
+
+// command line interface and constants
+#include "GQAP_CLI.h"
+
+// neighborhood
+#include "GQAP_ElementFlip_Neighbor.h"
+
+// possible neighborhood and local search definitions
+#include <neighborhood/moOrderNeighborhood.h>
+#include <neighborhood/moRndWithoutReplNeighborhood.h>
+#include <neighborhood/moDummyNeighborhood.h>
+
+#include <algo/moSimpleHC.h>
+#include <algo/moFirstImprHC.h>
+#include <algo/moDummyLS.h>
+
+// possible stopping criteria
+#include <continuator/moTimeContinuator.h>
+#include <continuator/moFitContinuator.h>
+#include <continuator/moIterContinuator.h>
+
+// possible alpha generators
+#include "GRASP_FixedAlpha.h"
+#include "GRASP_ReactiveAlpha.h"
+#include "GRASP_UniformRandomAlpha.h" 
+
+// for time measurement
+#include <time.h>
+
+// for results file handling
+#include <fstream>
+
+// implementation of an abstract GRASP Algorithm which uses the parameters set by the CLI
+// this makes every part optional / modular: initialization is the only thing required and could be random
+// local search an path-relinking are optional 
+class GQAP_GRASP_Algo {
+private:
+	// a copy of the parameters
+	parameters param;
+	
+	// the problem
+	GQAP * p;
+	
+	// strcuture for the best known solution
+	GQAP_Solution bestSol;
+	
+	// Neighborhood Size
+	unsigned NeighborhoodSize;
+	
+	// Evaluation Function
+	GQAP_Eval fullEval;
+	GQAP_ElementFlip_IncrEval * incrEval;
+	
+	// a pointer to the local search function
+	moLocalSearch<GQAP_ElementFlipIndex_Neighbor> * ls;
+	
+	// a pointer to the neighboorhood definition
+	moIndexNeighborhood<GQAP_ElementFlipIndex_Neighbor> * nh;
+	
+	// a pointer to the continuator (stopping criterion)
+	moContinuator<GQAP_ElementFlipIndex_Neighbor> * cont;
+	
+	// a pointer to an alpha-generator
+	GRASP_Alpha * alpha;
+
+	
+public:
+	// Constructors
+	GQAP_GRASP_Algo(parameters _param);
+	~GQAP_GRASP_Algo();
+	
+	// Runners
+	void Run(GQAP_Solution & _Sol, double & _Runtime);
+	void Run();
+	void RunTimeToTarget();
+	void GRASPIteration();
+};
+
+#endif // GQAP_GRASP_Algo_H
