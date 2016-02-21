9f20d0dc3b49a9f862ba87351b2ef0d0b84c04c6
Hagen Salewski <salewski@wiwi.uni-kl.de>
2016-02-21 11:19:33 +0100

    Refined tests for neighborhoor influence

--- tests/Time2Target-Cordeau/20-15-35/RunTest-20-15-35-LS-vs-Start.sh
+++ tests/Time2Target-Cordeau/20-15-35/RunTest-20-15-35-LS-vs-Start.sh
@@ -8,14 +8,14 @@ echo "GRASP Init, best improvement, greedy pool select, mixed PR-dir and ALL poo
 ./GRASP_Solver_T2T --seed=19141108 --problemFile="20-15-35.txt" -r=250 --resultsFile="Test-20-15-35-Start_LS.csv" --stoppingCriterion=v -v=1471896 --LSStrategy=b --StartSol=R --PRMeth=g --PRPoolSelect=a --PRDir=m --minAlpha=0 --maxAlpha=1 --numAlpha=10 --itsReCalcAlpha=2
 
 echo "Quick GRASP Init, best improvement, greedy pool select, mixed PR-dir and ALL pool selection"
-./GRASP_Solver_T2T --seed=19141108 --problemFile="20-15-35.txt" -r=250 --resultsFile="Test-20-15-35-Start_LS.csv" --stoppingCriterion=v -v=1471896 --LSStrategy=b --StartSol=R --PRMeth=g --PRPoolSelect=a --PRDir=m --minAlpha=0 --maxAlpha=1 --numAlpha=10 --itsReCalcAlpha=2 --QuickGRASP=1
+./GRASP_Solver_T2T --seed=19141108 --problemFile="20-15-35.txt" -r=250 --resultsFile="Test-20-15-35-Start_LS-q.csv" --stoppingCriterion=v -v=1471896 --LSStrategy=b --StartSol=R --PRMeth=g --PRPoolSelect=a --PRDir=m --minAlpha=0 --maxAlpha=1 --numAlpha=10 --itsReCalcAlpha=2 --QuickGRASP=1
 
 
 echo "Random Init, first improvement, greedy pool select, mixed PR-dir and ALL pool selection"
-./GRASP_Solver_T2T --seed=19141108 --problemFile="20-15-35.txt" -r=250 --resultsFile="Test-20-15-35-Start_LS.csv" --stoppingCriterion=v -v=1471896 --LSStrategy=b --StartSol=r --PRMeth=g --PRPoolSelect=a --PRDir=m
+./GRASP_Solver_T2T --seed=19141108 --problemFile="20-15-35.txt" -r=250 --resultsFile="Test-20-15-35-Start_LS.csv" --stoppingCriterion=v -v=1471896 --LSStrategy=f --StartSol=r --PRMeth=g --PRPoolSelect=a --PRDir=m
 
 echo "GRASP Init, first improvement, greedy pool select, mixed PR-dir and ALL pool selection"
-./GRASP_Solver_T2T --seed=19141108 --problemFile="20-15-35.txt" -r=250 --resultsFile="Test-20-15-35-Start_LS.csv" --stoppingCriterion=v -v=1471896 --LSStrategy=b --StartSol=R --PRMeth=g --PRPoolSelect=a --PRDir=m --minAlpha=0 --maxAlpha=1 --numAlpha=10 --itsReCalcAlpha=2
+./GRASP_Solver_T2T --seed=19141108 --problemFile="20-15-35.txt" -r=250 --resultsFile="Test-20-15-35-Start_LS.csv" --stoppingCriterion=v -v=1471896 --LSStrategy=f --StartSol=R --PRMeth=g --PRPoolSelect=a --PRDir=m --minAlpha=0 --maxAlpha=1 --numAlpha=10 --itsReCalcAlpha=2
 
 echo "Quick GRASP Init, first improvement, greedy pool select, mixed PR-dir and ALL pool selection"
-./GRASP_Solver_T2T --seed=19141108 --problemFile="20-15-35.txt" -r=250 --resultsFile="Test-20-15-35-Start_LS.csv" --stoppingCriterion=v -v=1471896 --LSStrategy=b --StartSol=R --PRMeth=g --PRPoolSelect=a --PRDir=m --minAlpha=0 --maxAlpha=1 --numAlpha=10 --itsReCalcAlpha=2 --QuickGRASP=1
+./GRASP_Solver_T2T --seed=19141108 --problemFile="20-15-35.txt" -r=250 --resultsFile="Test-20-15-35-Start_LS-q.csv" --stoppingCriterion=v -v=1471896 --LSStrategy=b --StartSol=R --PRMeth=g --PRPoolSelect=a --PRDir=m --minAlpha=0 --maxAlpha=1 --numAlpha=10 --itsReCalcAlpha=2 --QuickGRASP=1
